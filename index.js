6f66393a8cdb4d7e5542b86889e15377c7731d77
ljsimpkin <liamsimpkin@Liams-MacBook-Pro.local>
2021-01-08 18:56:46 +0000

    first, issue with _classCallCheck2 is not defined

---
+++ index.js
@@ -0,0 +1,22 @@
+import { Computer } from 'bitcoin-computer'
+
+class Counter {
+  constructor() { this.n = 0 }
+  inc() { this.n += 1 }
+}
+
+;(async () => {
+  const computer = new Computer({
+    seed: 'wet man custom turn card fatigue detail fetch express sauce reform mistake',
+    chain: 'BSV', // BSV or BCH
+    network: 'testnet', // testnet or livenet
+    path: "m/44'/0'/0'/0" // defaults to "m/44'/0'/0'/0"
+  })
+  const counter = await computer.new(Counter, [])
+  document.getElementById("el").innerHTML = `Counter is ${counter.n}`
+
+  await counter.inc()
+  document.getElementById("el").innerHTML = `Counter is ${counter.n}`
+})()
+
+console.log("hello")
